e0baab6d783579eaba9af3d562b82c227540cb88
ifuxas <rokaszilinskas09@gmail.com>
2024-05-02 21:33:33 +0300

    publish final

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="rg_tixify"
       options={{
         multiple: false,
         resourceType: "image",
